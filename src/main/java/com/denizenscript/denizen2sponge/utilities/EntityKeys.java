b5a453396e0df6a18e78d9e7040addf2fbe55da4
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-09-28 20:53:48 -0700

    support vector3d keys (EG velocity)

--- src/main/java/com/denizenscript/denizen2sponge/utilities/EntityKeys.java
+++ src/main/java/com/denizenscript/denizen2sponge/utilities/EntityKeys.java
@@ -6,6 +6,8 @@ import com.denizenscript.denizen2core.utilities.Action;
 import com.denizenscript.denizen2core.utilities.CoreUtilities;
 import com.denizenscript.denizen2core.utilities.ErrorInducedException;
 import com.denizenscript.denizen2sponge.tags.objects.FormattedTextTag;
+import com.denizenscript.denizen2sponge.tags.objects.LocationTag;
+import com.flowpowered.math.vector.Vector3d;
 import org.spongepowered.api.CatalogType;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.data.key.Key;
@@ -59,6 +61,9 @@ public class EntityKeys {
         else if (Integer.class.isAssignableFrom(clazz)) {
             return new IntegerTag(entity.getOrElse((Key<BaseValue<Integer>>) key, 0));
         }
+        else if (Vector3d.class.isAssignableFrom(clazz)) {
+            return new LocationTag(entity.getOrElse((Key<BaseValue<Vector3d>>) key, new Vector3d(0, 0, 0)));
+        }
         else if (Text.class.isAssignableFrom(clazz)) {
             return new FormattedTextTag(entity.getOrElse((Key<BaseValue<Text>>) key, Text.EMPTY));
         }
@@ -95,6 +100,9 @@ public class EntityKeys {
         }
         else if (Integer.class.isAssignableFrom(clazz)) {
             entity.offer(key, (int) IntegerTag.getFor(error, value).getInternal());
+        }
+        else if (Vector3d.class.isAssignableFrom(clazz)) {
+            entity.offer(key, LocationTag.getFor(error, value).getInternal().toVector3d());
         }
         else if (Text.class.isAssignableFrom(clazz)) {
             entity.offer(key, FormattedTextTag.getFor(error, value).getInternal());
