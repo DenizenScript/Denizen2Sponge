ef0814c1e3b4139150745a50b0920045bd8801ba
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2017-02-24 14:15:06 -0800

    fix pom and bad pasta

--- src/main/java/com/denizenscript/denizen2sponge/tags/objects/CuboidTag.java
+++ src/main/java/com/denizenscript/denizen2sponge/tags/objects/CuboidTag.java
@@ -91,7 +91,6 @@ public class CuboidTag extends AbstractTagObject {
         // @ReturnType ListTag
         // @Returns the location of all block locations in this cuboid. Optionally, specify a list of BlockType's to use.
         // @Note Partially covered blocks are counted.
-        // @Example "0,1,2,world" .world returns "world".
         // -->
         handlers.put("block_locations", (dat, obj) -> {
             List<BlockType> valids = null;
