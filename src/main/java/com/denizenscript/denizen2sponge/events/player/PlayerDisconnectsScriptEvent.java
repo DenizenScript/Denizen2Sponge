300b56aca2779b64c21a803a8411403a0779cc96
Xenmai <xenmai95@gmail.com>
2017-03-22 23:47:56 +0100

    Player Disconnects and Server Stops Events
    Also added Message Determination for Player Chats.

--- src/main/java/com/denizenscript/denizen2sponge/events/player/PlayerDisconnectsScriptEvent.java
+++ src/main/java/com/denizenscript/denizen2sponge/events/player/PlayerDisconnectsScriptEvent.java
@@ -2,47 +2,50 @@ package com.denizenscript.denizen2sponge.events.player;
 
 import com.denizenscript.denizen2core.events.ScriptEvent;
 import com.denizenscript.denizen2core.tags.AbstractTagObject;
+import com.denizenscript.denizen2core.tags.objects.BooleanTag;
 import com.denizenscript.denizen2sponge.Denizen2Sponge;
 import com.denizenscript.denizen2sponge.tags.objects.FormattedTextTag;
+import com.denizenscript.denizen2sponge.tags.objects.OfflinePlayerTag;
 import com.denizenscript.denizen2sponge.tags.objects.PlayerTag;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.filter.cause.Root;
-import org.spongepowered.api.event.message.MessageChannelEvent;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
 
 import java.util.HashMap;
 
-public class PlayerChatsScriptEvent extends ScriptEvent {
+public class PlayerDisconnectsScriptEvent extends ScriptEvent {
 
     // <--[event]
     // @Events
-    // player chats
+    // player disconnects
     //
     // @Updated 2017/03/22
     //
-    // @Cancellable true
+    // @Cancellable false
     //
     // @Group Player
     //
-    // @Triggers when a player sends a chat message.
+    // @Triggers when a player disconnects from the server.
     //
     // @Context
-    // player (PlayerTag) returns the player that sent the message.
-    // message (FormattedTextTag) returns the chat message.
+    // player (PlayerTag) returns the player that disconnected.
+    // message (FormattedTextTag) returns the message that will be broadcast to the server.
     //
     // @Determinations
-    // None.
+    // message (FormattedTextTag) to set the message displayed when a player disconnects.
+    // cancel_message (BooleanTag) to set whether the disconnect message is cancelled.
     // -->
 
     @Override
     public String getName() {
-        return "PlayerChats";
+        return "PlayerDisconnects";
     }
 
     @Override
     public boolean couldMatch(ScriptEventData data) {
-        return data.eventPath.startsWith("player chats");
+        return data.eventPath.startsWith("player disconnects");
     }
 
     @Override
@@ -54,7 +57,7 @@ public class PlayerChatsScriptEvent extends ScriptEvent {
 
     public FormattedTextTag message;
 
-    public MessageChannelEvent.Chat internal;
+    public ClientConnectionEvent.Disconnect internal;
 
     @Override
     public HashMap<String, AbstractTagObject> getDefinitions(ScriptEventData data) {
@@ -75,14 +78,12 @@ public class PlayerChatsScriptEvent extends ScriptEvent {
     }
 
     @Listener
-    public void onChat(MessageChannelEvent.Chat evt, @Root Player player) {
-        PlayerChatsScriptEvent event = (PlayerChatsScriptEvent) clone();
+    public void onDisconnect(ClientConnectionEvent.Disconnect evt, @Root Player player) {
+        PlayerDisconnectsScriptEvent event = (PlayerDisconnectsScriptEvent) clone();
         event.internal = evt;
         event.player = new PlayerTag(player);
-        event.message = new FormattedTextTag(evt.getRawMessage());
-        event.cancelled = evt.isCancelled();
+        event.message = new FormattedTextTag(evt.getMessage());
         event.run();
-        evt.setCancelled(event.cancelled);
     }
 
     @Override
@@ -91,6 +92,11 @@ public class PlayerChatsScriptEvent extends ScriptEvent {
             FormattedTextTag ftt = FormattedTextTag.getFor(this::error, value);
             message = ftt;
             internal.setMessage(ftt.getInternal());
+        }
+        else if (determination.equals("cancel_message")) {
+            // TODO: Context for this?
+            BooleanTag bt = BooleanTag.getFor(this::error, value);
+            internal.setMessageCancelled(bt.getInternal());
         }
         else {
             super.applyDetermination(errors, determination, value);
---
+++ src/main/java/com/denizenscript/denizen2sponge/events/server/ServerStopsScriptEvent.java
@@ -0,0 +1,84 @@
+package com.denizenscript.denizen2sponge.events.server;
+
+import com.denizenscript.denizen2core.events.ScriptEvent;
+import com.denizenscript.denizen2core.tags.AbstractTagObject;
+import com.denizenscript.denizen2core.tags.objects.IntegerTag;
+import com.denizenscript.denizen2core.tags.objects.TextTag;
+import com.denizenscript.denizen2core.utilities.debugging.Debug;
+import com.denizenscript.denizen2sponge.Denizen2Sponge;
+import com.denizenscript.denizen2sponge.tags.objects.FormattedTextTag;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
+import org.spongepowered.api.event.server.ClientPingServerEvent;
+import org.spongepowered.api.network.status.StatusClient;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+public class ServerStopsScriptEvent extends ScriptEvent {
+
+    // <--[event]
+    // @Events
+    // server stops
+    //
+    // @Updated 2017/03/22
+    //
+    // @Cancellable false
+    //
+    // @Group Server
+    //
+    // @Triggers when the server stops.
+    //
+    // @Context
+    // None.
+    //
+    // @Determinations
+    // None.
+    // -->
+
+    @Override
+    public String getName() {
+        return "ServerStops";
+    }
+
+    @Override
+    public boolean couldMatch(ScriptEventData data) {
+        return data.eventPath.startsWith("server stops");
+    }
+
+    @Override
+    public boolean matches(ScriptEventData data) {
+        return true;
+    }
+
+    public GameStoppingServerEvent internal;
+
+    @Override
+    public HashMap<String, AbstractTagObject> getDefinitions(ScriptEventData data) {
+        HashMap<String, AbstractTagObject> defs = super.getDefinitions(data);
+        return defs;
+    }
+
+    @Override
+    public void enable() {
+        Sponge.getEventManager().registerListeners(Denizen2Sponge.instance, this);
+    }
+
+    @Override
+    public void disable() {
+        Sponge.getEventManager().unregisterListeners(this);
+    }
+
+    @Listener
+    public void onServerStop(GameStoppingServerEvent evt) {
+        ServerStopsScriptEvent event = (ServerStopsScriptEvent) clone();
+        event.internal = evt;
+        event.run();
+    }
+
+    @Override
+    public void applyDetermination(boolean errors, String determination, AbstractTagObject value) {
+        super.applyDetermination(errors, determination, value);
+    }
+}
