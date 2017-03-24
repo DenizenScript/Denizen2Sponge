8df976d603373f0a3577f12e4bdd26c2b2ea3676
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2017-03-24 10:58:41 -0700

    Add alternate event helper options

--- src/main/java/com/denizenscript/denizen2sponge/events/D2SpongeEventHelper.java
+++ src/main/java/com/denizenscript/denizen2sponge/events/D2SpongeEventHelper.java
@@ -10,16 +10,24 @@ import org.spongepowered.api.entity.EntityType;
 public class D2SpongeEventHelper {
 
     public static boolean checkBlockType(BlockType btype, ScriptEvent.ScriptEventData data, Action<String> error) {
+        return checkBlockType(btype, data, error, "type");
+    }
+
+    public static boolean checkBlockType(BlockType btype, ScriptEvent.ScriptEventData data, Action<String> error, String tname) {
         // TODO: type_list as well?
-        return !data.switches.containsKey("type")
-                || BlockTypeTag.getFor(error, data.switches.get("type")).getInternal()
+        return !data.switches.containsKey(tname)
+                || BlockTypeTag.getFor(error, data.switches.get(tname)).getInternal()
                 .equals(btype);
     }
 
     public static boolean checkEntityType(EntityType etype, ScriptEvent.ScriptEventData data, Action<String> error) {
+        return checkEntityType(etype, data, error, "type");
+    }
+
+    public static boolean checkEntityType(EntityType etype, ScriptEvent.ScriptEventData data, Action<String> error, String tname) {
         // TODO: type_list as well?
-        return !data.switches.containsKey("type")
-                || EntityTypeTag.getFor(error, data.switches.get("type")).getInternal()
+        return !data.switches.containsKey(tname)
+                || EntityTypeTag.getFor(error, data.switches.get(tname)).getInternal()
                 .equals(etype);
     }
 }
