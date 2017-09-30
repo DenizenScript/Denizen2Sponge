9f097a8abd21b9c7a401f799816bbb6abbba1198
Xenmai <xenmai95@gmail.com>
2017-09-30 13:02:35 +0200

    Entity Specific Tags
    Boats, hanging entities, falling blocks, projectiles and explosives.

--- src/main/java/com/denizenscript/denizen2sponge/commands/server/AnnounceCommand.java
+++ src/main/java/com/denizenscript/denizen2sponge/commands/server/AnnounceCommand.java
@@ -8,7 +8,6 @@ import com.denizenscript.denizen2core.utilities.debugging.ColorSet;
 import com.denizenscript.denizen2sponge.Denizen2Sponge;
 import com.denizenscript.denizen2sponge.tags.objects.FormattedTextTag;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
 
 public class AnnounceCommand extends AbstractCommand {
